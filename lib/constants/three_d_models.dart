a59087e3b98307b26c13603d639d19ac284664e7
Axel Asa <axel.asa@ansu.co>
2024-07-27 21:21:56 +0300

    renaming 3d packages

--- lib/constants/three_d_models.dart
+++ lib/constants/three_d_models.dart
@@ -1,8 +1,8 @@
-const String videoCamera = 'assets/3d/video_camera.glb';
-const String camera = 'assets/3d/canon_dslr_camera.glb';
-const String drone = 'assets/3d/dji_drone_3.glb';
-const String streaming = 'assets/3d/cinema_camera.glb';
-const String marketing = 'assets/3d/social_media_icons.glb';
-const String studio = 'assets/3d/studio_umbrella_light.glb';
-const String events = 'assets/3d/podium.glb';
-const String publicRelations = 'assets/3d/handshake_-_ramadhan_series.glb';
+const String videoCamera = 'assets/three_d/video_camera.glb';
+const String camera = 'assets/three_d/canon_dslr_camera.glb';
+const String drone = 'assets/three_d/dji_drone_3.glb';
+const String streaming = 'assets/three_d/cinema_camera.glb';
+const String marketing = 'assets/three_d/social_media_icons.glb';
+const String studio = 'assets/three_d/studio_umbrella_light.glb';
+const String events = 'assets/three_d/podium.glb';
+const String publicRelations = 'assets/three_d/handshake_-_ramadhan_series.glb';
